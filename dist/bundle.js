d981a97273aad5eeb3674ee93fc098dc48964e47
jsiu <jonsiu826@gmail.com>
2020-11-02 22:30:32 -0800

    adding basketball falling func

--- dist/bundle.js
+++ dist/bundle.js
@@ -9,13 +9,17 @@ console.log("Hello from webpack")
 
 document.addEventListener("DOMContentLoaded", () => {
     let startButton = document.getElementById("start-button");
+    // let resetButton = document.getElementById("reset-button");
 
   startButton.addEventListener("click", () => { 
     document.getElementById("introduction").classList.add("hidden");
    })
 
-
+//    resetButton.addEventListener("click", () => { 
+//     game.restart();
+//   })
 })
+
 
 document.addEventListener("keydown", event => {
   if(event.key==="ArrowLeft"){moveLeft();}
@@ -25,7 +29,7 @@ document.addEventListener("keydown", event => {
 let basket = document.getElementById("basket");
 function moveLeft(){
     let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-    left -= 50;
+    left -= 100;
     if(left>=0){
         basket.style.left = left + "px";
     }
@@ -33,11 +37,41 @@ function moveLeft(){
 
 function moveRight(){
     let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-    left += 50;
+    left += 100;
     if(left<800){
         basket.style.left = left + "px";
     }
 }
+
+let basketball = document.getElementById("basketball");
+let counter = 0;
+basketball.addEventListener('animationiteration', () => {
+    let random = Math.floor(Math.random() * 8);
+    left = random * 100;
+    basketball.style.left = left + "px";
+    counter++;
+});
+
+setInterval(function(){
+    var characterLeft = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+    var blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
+    var blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
+    if(characterLeft!=blockLeft && blockTop<700 && blockTop>590){
+        alert("Game over. Score: " + counter);
+        // basketball.style.animation = "none";
+    }
+},1);
+
+// setInterval(function(){
+//     var characterLeft = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+//     var blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
+//     var blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
+//     if(characterLeft!=blockLeft && blockTop<700 && blockTop>550){
+//         alert("Game over. Score: " + counter);
+//         basketball.style.animation = "none";
+//     }
+// },1);
+
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
