a59fe5a27ded1560510a889c606ad4acda6e22e3
jsiu <jonsiu826@gmail.com>
2020-11-02 20:34:55 -0800

    adding basket and bomb

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,13 +6,38 @@
 /*! runtime requirements:  */
 console.log("Hello from webpack")
 
+
 document.addEventListener("DOMContentLoaded", () => {
     let startButton = document.getElementById("start-button");
 
   startButton.addEventListener("click", () => { 
     document.getElementById("introduction").classList.add("hidden");
    })
+
+
 })
+
+document.addEventListener("keydown", event => {
+  if(event.key==="ArrowLeft"){moveLeft();}
+  if(event.key==="ArrowRight"){moveRight();}
+});
+
+let basket = document.getElementById("basket");
+function moveLeft(){
+    let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+    left -= 50;
+    if(left>=0){
+        basket.style.left = left + "px";
+    }
+}
+
+function moveRight(){
+    let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+    left += 50;
+    if(left<800){
+        basket.style.left = left + "px";
+    }
+}
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 console.log("Hello from webpack")
 
+
 document.addEventListener("DOMContentLoaded", () => {
     let startButton = document.getElementById("start-button");
 
   startButton.addEventListener("click", () => { 
     document.getElementById("introduction").classList.add("hidden");
    })
+
+
 })
+
+document.addEventListener("keydown", event => {
+  if(event.key==="ArrowLeft"){moveLeft();}
+  if(event.key==="ArrowRight"){moveRight();}
+});
+
+let basket = document.getElementById("basket");
+function moveLeft(){
+    let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+    left -= 50;
+    if(left>=0){
+        basket.style.left = left + "px";
+    }
+}
+
+function moveRight(){
+    let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
+    left += 50;
+    if(left<800){
+        basket.style.left = left + "px";
+    }
+}
