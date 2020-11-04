7cbc6c6baa18279f6f6c654774911ce1a4f20dc1
jsiu <jonsiu826@gmail.com>
2020-11-04 15:33:02 -0800

    fixed reset func

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,6 +8,9 @@ console.log("Hello from webpack")
 
 const scoreDisplay = document.getElementById('score')
 const highscoreDisplay = document.getElementById('high-score');
+let basketball = document.getElementById("basketball");
+let score = 0;
+let highScore = 0;
 
 basketball.style.animation = "none";
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,13 +23,14 @@ document.addEventListener("DOMContentLoaded", () => {
     startGame();
    })
 
-//    resetButton.addEventListener("click", () => { 
-//     resetGame();
-//     document.getElementById("modal-content").classList.add("hidden");
-//   })
+   resetButton.addEventListener("click", () => { 
+       resetGame();
+       basketball.style.animation = "slide 1s infinite linear"
+       document.getElementById("modal").classList.add("hidden");
+  })
 })
 
-let highScore = 0;
+
 
 function startGame(){
     document.addEventListener("keydown", event => {
@@ -51,8 +55,8 @@ function startGame(){
         }
     }
     
-    let basketball = document.getElementById("basketball");
-    let score = 0;
+ 
+   
     
     basketball.addEventListener('animationiteration', () => {
         let random = Math.floor(Math.random() * 8);
@@ -69,14 +73,13 @@ function startGame(){
         let blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
         let blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
         if(characterLeft!=blockLeft && blockTop<700 && blockTop>590){
-            // alert("Game over. Score: " + counter);
             basketball.style.animation = "none";
+            document.getElementById("modal").classList.remove("hidden")
         }
     },1);
 }
 
 function resetGame(){
-    
     document.addEventListener("keydown", event => {
       if(event.key==="ArrowLeft"){moveLeft();}
       if(event.key==="ArrowRight"){moveRight();}
@@ -85,7 +88,7 @@ function resetGame(){
     let basket = document.getElementById("basket");
     function moveLeft(){
         let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-        left -= 100;
+        // left -= 100;
         if(left>=0){
             basket.style.left = left + "px";
         }
@@ -93,7 +96,7 @@ function resetGame(){
     
     function moveRight(){
         let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-        left += 100;
+        // left += 100;
         if(left<800){
             basket.style.left = left + "px";
         }
@@ -117,8 +120,7 @@ function resetGame(){
         let blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
         let blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
         if(characterLeft!=blockLeft && blockTop<700 && blockTop>590){
-            // alert("Game over. Score: " + counter);
-            // document.getElementById("modal").classList.remove("hidden");
+            document.getElementById("modal").classList.remove("hidden");
             // score=0;
             basketball.style.animation = "none";
         }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ console.log("Hello from webpack")
 
 const scoreDisplay = document.getElementById('score')
 const highscoreDisplay = document.getElementById('high-score');
+let basketball = document.getElementById("basketball");
+let score = 0;
+let highScore = 0;
 
 basketball.style.animation = "none";
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,13 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
     startGame();
    })
 
-//    resetButton.addEventListener("click", () => { 
-//     resetGame();
-//     document.getElementById("modal-content").classList.add("hidden");
-//   })
+   resetButton.addEventListener("click", () => { 
+       resetGame();
+       basketball.style.animation = "slide 1s infinite linear"
+       document.getElementById("modal").classList.add("hidden");
+  })
 })
 
-let highScore = 0;
+
 
 function startGame(){
     document.addEventListener("keydown", event => {
@@ -45,8 +49,8 @@ function startGame(){
         }
     }
     
-    let basketball = document.getElementById("basketball");
-    let score = 0;
+ 
+   
     
     basketball.addEventListener('animationiteration', () => {
         let random = Math.floor(Math.random() * 8);
@@ -63,14 +67,13 @@ function startGame(){
         let blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
         let blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
         if(characterLeft!=blockLeft && blockTop<700 && blockTop>590){
-            // alert("Game over. Score: " + counter);
             basketball.style.animation = "none";
+            document.getElementById("modal").classList.remove("hidden")
         }
     },1);
 }
 
 function resetGame(){
-    
     document.addEventListener("keydown", event => {
       if(event.key==="ArrowLeft"){moveLeft();}
       if(event.key==="ArrowRight"){moveRight();}
@@ -79,7 +82,7 @@ function resetGame(){
     let basket = document.getElementById("basket");
     function moveLeft(){
         let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-        left -= 100;
+        // left -= 100;
         if(left>=0){
             basket.style.left = left + "px";
         }
@@ -87,7 +90,7 @@ function resetGame(){
     
     function moveRight(){
         let left = parseInt(window.getComputedStyle(basket).getPropertyValue("left"));
-        left += 100;
+        // left += 100;
         if(left<800){
             basket.style.left = left + "px";
         }
@@ -111,8 +114,7 @@ function resetGame(){
         let blockLeft = parseInt(window.getComputedStyle(basketball).getPropertyValue("left"));
         let blockTop = parseInt(window.getComputedStyle(basketball).getPropertyValue("top"));
         if(characterLeft!=blockLeft && blockTop<700 && blockTop>590){
-            // alert("Game over. Score: " + counter);
-            // document.getElementById("modal").classList.remove("hidden");
+            document.getElementById("modal").classList.remove("hidden");
             // score=0;
             basketball.style.animation = "none";
         }
